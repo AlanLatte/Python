0dc837f45262b1d168f6bc1f177bb4e114206ce8
AlanLatte <shukurovfoteh@gmail.com>
2022-02-25 15:37:35 +0300

    [+] Написал модуль для записи пакетов в контейнеры

---
+++ app/pkg/containter_dispatcher/__init__.py
@@ -0,0 +1,18 @@
+from typing import List
+from .wire_packages import wire_packages
+
+
+def register_container(pkg_name: str, packages: List[str] = None) -> None:
+    """Function for register all containers.
+
+    Args:
+        pkg_name: __name__ of main package.
+        packages: Optional array of packages to which the injector will be available.
+            Default: `app.internal`
+    Returns:
+        None
+    """
+    if packages is None:
+        packages = ["app.internal"]
+    ...
+
