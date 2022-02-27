49a7e561876b4223874392d39c2a08c43bd0d869
AlanLatte <shukurovfoteh@gmail.com>
2022-02-27 23:07:22 +0300

    [+] Создал базовый абстрактный репозиторйи

---
+++ app/internal/pkg/repository/repository.py
@@ -0,0 +1,27 @@
+from abc import ABC
+from typing import List, TypeVar
+from app.pkg.models.base import Model
+
+__all__ = ["Repository", "BaseRepository"]
+
+
+BaseRepository = TypeVar("BaseRepository", bound="Repository")
+
+
+class Repository(ABC):
+    """Base repository interface."""
+
+    async def create(self, cmd: Model) -> Model:
+        raise NotImplementedError
+
+    async def read(self, query: Model) -> Model:
+        raise NotImplementedError
+
+    async def read_all(self) -> List[Model]:
+        raise NotImplementedError
+
+    async def update(self, cmd: Model) -> Model:
+        raise NotImplementedError
+
+    async def delete(self, cmd: Model) -> Model:
+        raise NotImplementedError
