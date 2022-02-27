1e40f0a1aba07e7ac2b410d28ef339eca2a19c54
AlanLatte <shukurovfoteh@gmail.com>
2022-02-27 23:06:52 +0300

    [+] Создал коннектор к бд

---
+++ app/internal/pkg/repository/postgresql/connection.py
@@ -0,0 +1,21 @@
+from contextlib import asynccontextmanager
+
+from aiopg.connection import Cursor
+from dependency_injector.wiring import Provide, inject
+from psycopg2.extras import RealDictCursor
+
+from app.pkg.connectors import Connectors
+from app.pkg.connectors.postgresql import Postgresql
+
+__all__ = ["get_connection"]
+
+
+@asynccontextmanager
+@inject
+async def get_connection(
+    postgresql: Postgresql = Provide[Connectors.postgres],
+) -> Cursor:
+    """Get async connection to postgresql of pool."""
+    async with postgresql.get_connect() as connection:
+        async with (await connection.cursor(cursor_factory=RealDictCursor)) as cur:
+            yield cur
