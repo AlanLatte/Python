094c7888b1c9e937a5213551d6f83437614102c7
AlanLatte <shukurovfoteh@gmail.com>
2022-03-01 13:29:18 +0300

    [*] Изменил тип pydantic.AnyUrl -> str, потому что localhost не проходит проверку.

--- app/pkg/settings/settings.py
+++ app/pkg/settings/settings.py
@@ -1,45 +1,39 @@
 """
 Module for load settings form `.env` or
-    if server running with parameter `dev` from `.env.dev`
+if server running with parameter `dev` from `.env.dev`
 """
 
 from functools import lru_cache
 
-import pydantic
 from dotenv import find_dotenv
 from pydantic.env_settings import BaseSettings
+from pydantic.types import PositiveInt, SecretStr
 
 __all__ = ["Settings", "get_settings"]
-
-from pydantic.types import PositiveInt, SecretStr
 
 
 class _Settings(BaseSettings):
     class Config:
         """Configuration of settings."""
 
-        #: str: env file encoding.  #Unresolved reference
+        #: str: env file encoding.
         env_file_encoding = "utf-8"
-        #: str: allow custom fields in model.  #Unresolved reference
+        #: str: allow custom fields in model.
         arbitrary_types_allowed = True
 
 
 class Settings(_Settings):
     """Server settings. Formed from `.env` or `.env.dev`."""
 
-    # Postgres
-    POSTGRES_HOST: pydantic.AnyUrl
+    POSTGRES_HOST: str
     POSTGRES_PORT: PositiveInt
     POSTGRES_USER: str
     POSTGRES_PASSWORD: SecretStr
     POSTGRES_DATABASE_NAME: str
 
-    # Redis
-    REDIS_HOST: pydantic.AnyUrl
+    REDIS_HOST: str
     REDIS_PORT: PositiveInt
     REDIS_PASSWORD: SecretStr
-
-
 
 
 @lru_cache()
