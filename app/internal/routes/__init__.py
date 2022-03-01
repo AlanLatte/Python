755f55bbc88dd4db485bd8ede66e432351379d34
AlanLatte <shukurovfoteh@gmail.com>
2022-03-01 13:25:38 +0300

    [+] Регистрирую роуты в модель Rotes

---
+++ app/internal/routes/__init__.py
@@ -0,0 +1,9 @@
+"""Global point for collected routers."""
+
+from app.internal.pkg.models import Routes
+from app.internal.routes import user
+
+__all__ = ["__routes__"]
+
+
+__routes__ = Routes(routers=(user.router,))
