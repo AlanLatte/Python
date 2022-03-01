c729cf60b2b73e54e8ebe202bf9b73291cf05093
AlanLatte <shukurovfoteh@gmail.com>
2022-03-01 13:24:47 +0300

    [+] Добавил документацию к функциям работы с паролями

--- app/internal/pkg/password/password.py
+++ app/internal/pkg/password/password.py
@@ -1,3 +1,5 @@
+"""Module for crypt and decrypt password."""
+
 import bcrypt
 from pydantic import SecretBytes
 
@@ -5,9 +7,27 @@ __all__ = ["crypt_password", "check_password"]
 
 
 def crypt_password(password: bytes) -> bytes:
+    """Crypt password.
+
+    Args:
+        password: Raw password.
+
+    Returns:
+        Crypt password used bcrypt algorythm.
+    """
     return bcrypt.hashpw(password, bcrypt.gensalt())
 
 
 def check_password(password: SecretBytes, hashed: SecretBytes) -> bool:
+    """Check equality of encrypted and raw password.
+
+    Args:
+        password: Raw password.
+        hashed: Hashed password.
+
+    Returns:
+        True if equality check passed, False otherwise.
+
+    """
     return bcrypt.checkpw(password.get_secret_value(), hashed.get_secret_value())
 
