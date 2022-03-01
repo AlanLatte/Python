a07983c2341ee5c8b700a724773ad10f621766e5
AlanLatte <shukurovfoteh@gmail.com>
2022-03-01 13:25:50 +0300

    [+] Создал функция пер-аппа сервера

---
+++ app/configuration/events.py
@@ -0,0 +1,14 @@
+"""``on_startup`` function will be called when server trying to start."""
+
+
+async def on_startup():
+    """Run code on server startup.
+
+    Warnings:
+        **Don't use this function for insert default data in database.
+        For this action we have scripts/migrate.py.**
+
+    Returns:
+        None
+    """
+    pass
