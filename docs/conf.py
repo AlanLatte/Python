b3abec54542d16c603904b5ea85d41717c290999
AlanLatte <shukurovfoteh@gmail.com>
2022-03-01 13:23:14 +0300

    [+] Создал настройки для Sphinx конфигурации

---
+++ docs/conf.py
@@ -0,0 +1,81 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
+# -- Project information -----------------------------------------------------
+
+project = "Architecture template"
+copyright = "2022, Alan Latte"
+author = "Alan Latte"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx-pydantic",
+]
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = True
+napoleon_use_ivar = True
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+add_module_names = False
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = []
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+autodoc_member_order = "alphabetical"
+autoapi_ignore = ["*tests*", "*migrations*"]
+autoapi_options = [
+    "members",
+    "undoc-members",
+    'private-members',
+    'special-members',
+    "show-inheritance",
+    "show-module-summary",
+    "imported-members",
+]
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+html_show_sphinx = False
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = []
---
+++ docs/index.rst
@@ -0,0 +1,18 @@
+..
+
+Hello!
+At first, you *must* ...
+
+==============================================================
+
+.. toctree::
+   :maxdepth: 3
+
+   modules
+   app
+
+Indices and tables
+==================
+
+* :ref:`modindex`
+* :ref:`search`
