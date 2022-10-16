fa6881581b82b86e198679fef0f9317a810d1343
NitaKai <BWG.it251@gmail.com>
2022-10-16 17:55:36 +0300

    [+] logger

---
+++ app/pkg/logger/logger.py
@@ -0,0 +1,38 @@
+import logging
+
+from pathlib import Path
+
+from logging.handlers import RotatingFileHandler
+from app.pkg.settings import settings
+
+
+_log_format = (
+    "%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%("
+    "funcName)s(%(lineno)d) - %(message)s "
+)
+
+
+def get_file_handler(file_name):
+    Path(file_name).absolute().parent.mkdir(exist_ok=True, parents=True)
+    file_handler = RotatingFileHandler(
+        filename=file_name,
+        maxBytes=5242880,
+        backupCount=10,
+    )
+    file_handler.setFormatter(logging.Formatter(_log_format))
+    return file_handler
+
+
+def get_stream_handler():
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter(_log_format))
+    return stream_handler
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    file_path = str(Path(settings.LOGGER_FILE_PATH).absolute())
+    logger.addHandler(get_file_handler(file_name=file_path))
+    logger.addHandler(get_stream_handler())
+    logger.setLevel(settings.LOGGER_LEVEL)
+    return logger
