544cfa6a5d1891dc303e3726ac7a158d08ca0b30
AlanLatte <shukurovfoteh@gmail.com>
2022-12-01 14:34:31 +0300

    [+] Добавил UserRoleRepository в фикстуры

--- tests/fixtures/repository/postgresql/repositories.py
+++ tests/fixtures/repository/postgresql/repositories.py
@@ -1,8 +1,14 @@
 import pytest
 
 from app.internal.repository.postgresql.user import UserRepository
+from app.internal.repository.postgresql.user_roles import UserRoleRepository
 
 
 @pytest.fixture()
 def user_repository() -> UserRepository:
     return UserRepository()
+
+
+@pytest.fixture()
+def user_role_repository() -> UserRepository:
+    return UserRoleRepository()
