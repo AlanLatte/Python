602ca88abb87b53bdb25eaad5d69fad3584001a6
AlanLatte <shukurovfoteh@gmail.com>
2022-12-01 05:13:33 +0300

    [+] Добавил обработку pytest хука инициализации сессии. Определяю окружение как тестовое. R: Тесты должны проводиться с использованием базы данных. Что бы настроить окружение, происходит вызов `set_environment`

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,19 @@
+from app.configuration import __containers__
+from app.pkg.connectors import Connectors
+
 pytest_plugins = [
     "tests.fixtures.repository.postgresql.postgresql_fixtures",
     "tests.fixtures.repository.postgresql.repositories",
+    "tests.fixtures.repository.postgresql.user",
     "tests.fixtures.settings_fixtures",
+    "tests.fixtures.models.user",
     # path to module with fixtures.
 ]
+
+
+def pytest_sessionstart(session):
+    _ = session
+
+    __containers__.set_environment(
+        connector_class=Connectors, pkg_name=__name__, testing=True
+    )
