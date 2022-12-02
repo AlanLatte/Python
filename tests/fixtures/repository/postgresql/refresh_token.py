025a8cdb45c4bae22b9588812e0884b1ec52ba18
AlanLatte <shukurovfoteh@gmail.com>
2022-12-02 20:27:03 +0300

    [+] refresh token моковые данные

---
+++ tests/fixtures/repository/postgresql/refresh_token.py
@@ -0,0 +1,78 @@
+from uuid import uuid4
+
+import pytest
+
+from app.pkg import models
+from app.pkg.models import CreateJWTRefreshTokenCommand, JWTRefreshToken
+
+
+@pytest.fixture
+def first_refresh_token():
+    return uuid4().__str__()
+
+
+@pytest.fixture
+def second_refresh_token():
+    return uuid4().__str__()
+
+
+@pytest.fixture
+def first_fingerprint():
+    return uuid4().__str__()
+
+
+@pytest.fixture
+def second_fingerprint():
+    return uuid4().__str__()
+
+
+@pytest.fixture
+def first_refresh_token_response(
+    insert_first_user: models.User,
+    first_refresh_token: models.JWTRefreshToken,
+    first_fingerprint: str,
+):
+    return JWTRefreshToken(
+        user_id=insert_first_user.id,
+        refresh_token=first_refresh_token,
+        fingerprint=first_fingerprint,
+    )
+
+
+@pytest.fixture
+def second_refresh_token_response(
+    insert_postgres_second_user: models.User,
+    second_refresh_token: models.JWTRefreshToken,
+    second_fingerprint: str,
+):
+    return JWTRefreshToken(
+        user_id=insert_postgres_second_user.id,
+        refresh_token=second_refresh_token,
+        fingerprint=second_fingerprint,
+    )
+
+
+@pytest.fixture
+def insert_first_refresh_token(
+    insert_first_user: models.User,
+    first_refresh_token: models.JWTRefreshToken,
+    first_fingerprint: str,
+):
+    return CreateJWTRefreshTokenCommand(
+        user_id=insert_first_user.id,
+        refresh_token=first_refresh_token,
+        fingerprint=first_fingerprint,
+    )
+
+
+@pytest.fixture
+def insert_second_refresh_token(
+    insert_second_user: models.User,
+    second_refresh_token: models.JWTRefreshToken,
+    second_fingerprint: str,
+):
+    return CreateJWTRefreshTokenCommand(
+        user_id=insert_second_user.id,
+        refresh_token=second_refresh_token,
+        fingerprint=second_fingerprint,
+    )
