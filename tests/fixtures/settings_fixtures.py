8e53458658f51fb594cefeb87a3e1df39e41f8bd
AlanLatte <shukurovfoteh@gmail.com>
2022-12-02 20:21:32 +0300

    [+] Фикстура для получение настроек

--- tests/fixtures/settings_fixtures.py
+++ tests/fixtures/settings_fixtures.py
@@ -1,17 +1,12 @@
 import pytest
 
-from app.configuration import __containers__
-from app.pkg.settings.settings import get_settings
-
-_settings = get_settings("../../.env")
+from app.pkg.settings import settings as _settings
 
 
 @pytest.fixture()
-async def settings(wire):
+async def settings():
+    if _settings.POSTGRES_DATABASE_NAME.startswith("test_"):
+        return _settings
+
     _settings.POSTGRES_DATABASE_NAME = f"test_{_settings.POSTGRES_DATABASE_NAME}"
     return _settings
-
-
-@pytest.fixture(autouse=True)
-async def wire():
-    __containers__.wire_packages(pkg_name=__name__)
