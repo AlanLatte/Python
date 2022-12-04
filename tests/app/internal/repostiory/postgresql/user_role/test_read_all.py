163820c3329f9435a9f6b338be3122e888a7b50d
AlanLatte <shukurovfoteh@gmail.com>
2022-12-04 19:52:26 +0300

    [~] Добавил типизацию для тестов

--- tests/app/internal/repostiory/postgresql/user_role/test_read_all.py
+++ tests/app/internal/repostiory/postgresql/user_role/test_read_all.py
@@ -1,6 +1,6 @@
 import pytest
+from app.internal.repository.postgresql.user_roles import UserRoleRepository
 
-
-async def test_notimplemented_error(user_role_repository):
+async def test_notimplemented_error(user_role_repository: UserRoleRepository):
     with pytest.raises(NotImplementedError):
         await user_role_repository.read_all()
--- tests/app/internal/repostiory/postgresql/user_role/test_update.py
+++ tests/app/internal/repostiory/postgresql/user_role/test_update.py
@@ -1,8 +1,9 @@
 import pytest
 
+from app.internal.repository.postgresql.user_roles import UserRoleRepository
 from app.pkg.models.base import Model
 
 
-async def test_notimplemented_error(user_role_repository):
+async def test_notimplemented_error(user_role_repository: UserRoleRepository):
     with pytest.raises(NotImplementedError):
         await user_role_repository.update(cmd=Model)
