f6ce933e2c60d88358e9225126043405beb55b19
AlanLatte <shukurovfoteh@gmail.com>
2022-12-20 15:15:55 +0300

    [*] Тест не возвращает ошибку
    R: В sql запросе есть `on conflict do nothing`
    FB: Убрать `pytest.raises`

--- tests/app/internal/repostiory/postgresql/user_role/test_create.py
+++ tests/app/internal/repostiory/postgresql/user_role/test_create.py
@@ -2,16 +2,14 @@ import pytest
 
 from app.pkg import models
 from app.pkg.models.base import Model
-from app.pkg.models.exceptions.repository import EmptyResult
 from app.internal.repository.postgresql.user_roles import UserRoleRepository
 
 
 async def test_correct_insert_models(user_role_repository: UserRoleRepository):
-    with pytest.raises(EmptyResult):
-        for role in models.UserRole:
-            await user_role_repository.create(
-                cmd=models.CreateUserRoleCommand(role_name=role.value)
-            )
+    for role in models.UserRole:
+        await user_role_repository.create(
+            cmd=models.CreateUserRoleCommand(role_name=role.value)
+        )
 
 
 async def test_correct(user_role_repository: UserRoleRepository):
