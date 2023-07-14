3df4f5568be607d15ab02784d31cdf04e56e6a35
AlanLatte <shukurovfoteh@gmail.com>
2023-07-14 03:48:05 +0300

    [-] Удалил лишние коннекторы

--- app/pkg/connectors/__init__.py
+++ app/pkg/connectors/__init__.py
@@ -4,11 +4,9 @@ from dependency_injector import containers, providers
 
 from app.pkg.settings import settings
 
-from .mysql import Mysql
 from .postgresql import Postgresql
-from .sqlite import SQLite
 
-__all__ = ["Connectors", "SQLite", "Postgresql", "Mysql"]
+__all__ = ["Connectors", "Postgresql"]
 
 
 class Connectors(containers.DeclarativeContainer):
@@ -27,13 +25,3 @@ class Connectors(containers.DeclarativeContainer):
         port=configuration.POSTGRES.PORT,
         database_name=configuration.POSTGRES.DATABASE_NAME,
     )
-
-    # sqlite = providers.Factory(SQLite, sqlite_path=configuration.SQLITE_PATH)
-    # mysql = providers.Factory(
-    #     Mysql,
-    #     username=configuration.POSTGRES_USER,
-    #     password=configuration.POSTGRES_PASSWORD,
-    #     host=configuration.POSTGRES_HOST,
-    #     port=configuration.POSTGRES_PORT,
-    #     database_name=configuration.POSTGRES_DATABASE_NAME,
-    # )
