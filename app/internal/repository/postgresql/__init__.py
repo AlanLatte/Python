89a4372331967202b7e60e57c2a21b25101d22a8
AlanLatte <shukurovfoteh@gmail.com>
2023-09-26 00:40:38 +0300

    [~] Сменил относительные импорты на абсолютные

--- app/internal/repository/postgresql/__init__.py
+++ app/internal/repository/postgresql/__init__.py
@@ -1,11 +1,13 @@
 from dependency_injector import containers, providers
 
-from .refresh_tokens import JWTRefreshTokenRepository
-from .user import UserRepository
-from .user_roles import UserRoleRepository
+from app.internal.repository.postgresql.refresh_tokens import JWTRefreshTokenRepository
+from app.internal.repository.postgresql.user import UserRepository
+from app.internal.repository.postgresql.user_roles import UserRoleRepository
 
 
 class Repositories(containers.DeclarativeContainer):
+    """Container for postgresql repositories."""
+
     user_repository = providers.Factory(UserRepository)
     refresh_token_repository = providers.Factory(JWTRefreshTokenRepository)
     user_role_repository = providers.Factory(UserRoleRepository)
